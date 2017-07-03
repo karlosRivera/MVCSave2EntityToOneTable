fa62801cb187d4a935b2c8a42ead570ca700134a
tridip-bba <tridip@bba-reman.com>
2017-07-04 00:25:11 +0530

    single and multiple entity add completed

--- MVCSave2EntityToOneTable/Controllers/HomeController.cs
+++ MVCSave2EntityToOneTable/Controllers/HomeController.cs
@@ -13,11 +13,43 @@ namespace MVCSave2EntityToOneTable.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult SaveSingleData()
         {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
+            using (var ctx = new TestEFContext())
+            {
+                Employee emp = new Employee() { Name = "New employee1", Salary=2000 };
+
+                ctx.Employees.Add(emp);
+                ctx.SaveChanges();
+            }
+
+            ViewBag.Message = "Single employee data added.";
+            return View("Index");
+        }
+
+        public ActionResult SaveMultipleData()
+        {
+
+            List<Employee> oEmp = new List<Employee>
+            {
+                new Employee{Name="New employee2", Salary=5000},
+                new Employee{Name="New employee3", Salary=6000},
+                new Employee{Name="New employee4", Salary=7000}
+            };
+
+            using (var ctx = new TestEFContext())
+            {
+                foreach (Employee emp in oEmp)
+                {
+                    ctx.Employees.Add(emp);
+                }
+                ctx.SaveChanges();
+            }
+
+            ViewBag.Message = "Multiple employee data added.";
+
+            return View("About");
         }
 
         public ActionResult Contact()
